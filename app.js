3118c25599c601691ee3d97a72ef0a9a261bec79
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-01 16:43:05 +0100

    adds next event and displaying of next profile

--- app.js
+++ app.js
@@ -28,6 +28,23 @@ const data = [
 
 const profiles = profileIterator(data);
 
+// Next Event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+// Next profile display
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+
+  document.getElementById('profileDisplay').innerHTML = `
+    <ul class="list-group">
+      <li class="list-group-item">Name: ${currentProfile.name}</li>
+      <li class="list-group-item">Age: ${currentProfile.age}</li>
+      <li class="list-group-item">Location: ${currentProfile.location}</li>
+      <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+    </ul>
+    `;
+}
+
 // Profile Iterator
 function profileIterator(profiles) {
   let nextIndex = 0;
